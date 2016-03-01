7b037c274b54a616cae435fd83d05ce044fe0eb5
Patrick Ofilada <pcofilada@gmail.com>
2016-03-01 16:17:46 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# BookList
+
+###Getting Started###
+
+```
+	> git clone git@github.com:pcofilada/BookList.git
+	> cd BookList
+	> npm install
+	> npm start
+```
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Learning React in Easy way', pages: 50 },
+    { title: 'React 101', pages: 10 }
+  ];
+}
